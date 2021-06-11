2d62d2cbe8b61104b2bf8f7c4377e9ec1352a30c
hoxirious <giahy33@gmail.com>
2021-06-10 19:14:47 -0600

    initial commit

---
+++ course.cpp
@@ -0,0 +1,17 @@
+#include "course.h"
+
+int Course::find_student(string id) {}
+
+Course::Course() {}
+
+Course::Course(string id, Date s, Date e, int max) {}
+
+string Course::get_cid() {}
+
+void Course::enroll(Student *S_obj) {}
+
+void Course::withdraw(string uid) {}
+
+void Course::update_grade(string uid, double g) {}
+
+string Course::course_info() {}
